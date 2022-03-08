0b182ee385f3bbdc314b278254bd715b63302d4d
MayPhoo-Github <mayphoowai14@gmail.com>
2022-03-08 10:07:55 +0630

    [main]/first commit

---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is vue animation practice project.</p>
+    <p>In this project, I also used GSAP for transition group fade-in.</p>
+    <p>Reference from "Net Ninja"</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  setup() {
+
+  }
+}
+</script>
+
+<style>
+  .about {
+    max-width: 600px;
+    margin: 20px auto;
+  }
+</style>
